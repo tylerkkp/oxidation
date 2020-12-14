beb597d7b68ec100bfb634945b1aaaec3496c352
Tyler Kuromiya Parker <tylerkkp@gmail.com>
2020-12-13 21:04:08 -0800

    Added countdown to test function using thread::sleep()

--- input_output.rs
+++ input_output.rs
@@ -1,9 +1,28 @@
+use std::{thread, time};
 
-// copied from tutorialspoint for testing purposes
 fn main(){
    let mut line = String::new();
    println!("What is your name?");
    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+
+   let counttime = time::Duration::from_millis(500);
+
+   let countdown = vec!["1...",
+                        "2...",
+                        "3...",
+                        "4...",
+                        "5...",
+                        "6...",
+                        "7...",
+                        "8...",
+                        "9...",
+                        "10!!!"];
+   for i in &countdown {
+        println!("{}", i);
+        // sleep for 500 milliseconds
+        thread::sleep(counttime);
+   }
+
    println!("Hello , {}", line);
    println!("no of bytes read , {}", b1);
 }
